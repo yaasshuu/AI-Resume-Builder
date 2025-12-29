270d50de08a39127cf94c7ecc8fbe70fe24d5990
Yashasvi Tiwari <161428817+yaasshuu@users.noreply.github.com>
2025-12-29 13:27:40 +0530

    Update the app configs

--- client/src/app/configs/api.js
+++ client/src/app/configs/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://ai-resume-builder-backend-ux19.onrender.com"
+  baseURL: "https://ai-resume-builder-backend-lalu.onrender.com"
 });
 
 export default api;
